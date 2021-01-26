f5e6027cfbdf70674b0ccf79c3667467da2866d0
matintosh <matias@upshow.tv>
2021-01-26 17:14:45 -0300

    Fixing import error.

--- backend/coinster-wallets/app/__init__.py
+++ backend/coinster-wallets/app/__init__.py
@@ -1,10 +1,8 @@
 import os
 
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 
-from app.modules.daemon.consumer import channel
-from app.modules.wallet.controllers import mod_wallet as wallet_modules
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -12,11 +10,14 @@ app.config.from_object('common.config')
 
 db = SQLAlchemy(app)
 
+from app.modules.wallet.controllers import mod_wallet as wallet_modules
 
 app.register_blueprint(wallet_modules)
 
 db.create_all()
 
+
+from app.modules.daemon.consumer import channel
 
 RUN_QUEUE = os.environ.get('RUN_QUEUE', 'False') 
 
